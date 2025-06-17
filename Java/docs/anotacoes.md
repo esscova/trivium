e8c44508fc1d51e3dfe5daf4d5b5cec5df1c97fc
wellington moreira <wsantos08@hotmail.com>
2025-06-17 15:36:22 -0300

    hello Java

---
+++ Java/docs/anotacoes.md
@@ -0,0 +1,5 @@
+# Progresso de Estudos em Java
+
+## ✅ Semana 1
+- [x] Instalar NetBeans
+- [x] Criar primeiro projeto Java
---
+++ Java/projetos/helloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hellojava;
+
+/**
+ *
+ * @author escova
+ */
+public class HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+    
+}
