b2a511cd42d087244a37d52f3e1df3cb41d720c2
wellington moreira <wsantos08@hotmail.com>
2025-06-24 09:56:43 -0300

    feat: mini projeto java - ficha pessoal

---
+++ Java/projetos/fichaPessoal/src/fichapessoal/FichaPessoal.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fichapessoal;
+
+/**
+ *
+ * @author escova
+ */
+public class FichaPessoal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // variaveis
+        String nome = "Maria Silva";
+        int idade = 22;
+        double altura = 1.65;
+        char primeiraLetraNome = 'M';
+        boolean estudante = true;
+        
+        // saida
+        System.out.println("=== FICHA PESSOAL ===");
+        System.out.println("Nome: "+ nome);
+        System.out.println("Idade: "+idade+" anos");
+        System.out.println("Altura: "+altura+"m");
+        System.out.println("Inicial: "+primeiraLetraNome);
+        System.out.println("Estudante: "+estudante);
+    }
+    í
+}
